237051dcf648cc906d4b45bccfcb6bdadbad63db
bhespiritu <lozman1@elne.org>
2023-09-25 23:26:43 -0400

    redo boilerplate code

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+
+import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import {CardDomainModule} from "../card-domain/card-domain.module";
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, CardDomainModule]
+})
+export class HomeModule {}
