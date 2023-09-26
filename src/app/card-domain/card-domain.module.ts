0e26f5f4449e84a62fde7cdbee3c96ce679ca358
bhespiritu <lozman1@elne.org>
2023-09-25 23:34:38 -0400

    begin dragging logic

--- src/app/card-domain/card-domain.module.ts
+++ src/app/card-domain/card-domain.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CardDomainComponent} from "./card-domain.component";
 import {MatCardModule} from "@angular/material/card";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 
 
@@ -10,6 +11,7 @@ import {MatCardModule} from "@angular/material/card";
   imports: [
     CommonModule,
     MatCardModule,
+    DragDropModule,
   ],
   exports: [
     CardDomainComponent
